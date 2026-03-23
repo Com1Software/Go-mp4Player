84eb8a38de0b64c3424b3dc01b978d34d5098b1d
Lenovo 15 <information@com1software.com>
2026-03-23 10:16:09 -0400

    update

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"io"
 	"math/rand/v2"
 	"os/exec"
 	"path/filepath"
@@ -13,8 +12,6 @@ import (
 
 	"fmt"
 	"os"
-
-	asciistring "github.com/Com1Software/Go-ASCII-String-Package"
 )
 
 type Tags struct {
@@ -134,69 +131,6 @@ func main() {
 		}
 
 	}
-}
-
-func fileNameWithoutExtension(fileName string) string {
-	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
-}
-func copy(src, dst string) (int64, error) {
-	sourceFileStat, err := os.Stat(src)
-	if err != nil {
-		return 0, err
-	}
-
-	if !sourceFileStat.Mode().IsRegular() {
-		return 0, fmt.Errorf("%s is not a regular file", src)
-	}
-
-	source, err := os.Open(src)
-	if err != nil {
-		return 0, err
-	}
-	defer source.Close()
-
-	destination, err := os.Create(dst)
-	if err != nil {
-		return 0, err
-	}
-	defer destination.Close()
-	nBytes, err := io.Copy(destination, source)
-	return nBytes, err
-}
-
-func fixFileName(fileName string) string {
-	newName := ""
-	chr := ""
-	ascval := 0
-
-	for x := 0; x < len(fileName); x++ {
-		chr = fileName[x : x+1]
-		ascval = asciistring.StringToASCII(chr)
-		switch {
-		case ascval < 45:
-		case ascval == 64:
-		case ascval == 92:
-		case ascval == 96:
-		case ascval > 122:
-		default:
-			newName = newName + chr
-		}
-	}
-
-	err := os.Rename(fileName, newName)
-	if err != nil {
-		fmt.Println("Error renaming file:", err)
-	}
-	return newName
-}
-
-func ValidFileType(fileExt string) bool {
-	rtn := false
-	switch {
-	case fileExt == ".mp4":
-		rtn = true
-	}
-	return rtn
 }
 
 func getDuration(ffprobePath, file string) int {
